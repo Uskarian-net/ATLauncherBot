9d91a7a62867f188e3af970850584f2e6819610b
RyanTheAllmighty <ryantheallmighty@atlauncher.com>
2015-08-02 15:28:17 +1000

    Add commands

---
+++ commands/mcStatusCommand.js
@@ -0,0 +1,60 @@
+/*
+ * ATLauncherBot - https://github.com/ATLauncher/ATLauncherBot
+ * Copyright (C) 2015 ATLauncher
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+'use strict';
+
+var connection = require('../inc/connection');
+var request = require('request');
+var _ = require('lodash');
+
+module.exports.enabled = true;
+
+module.exports.name = 'mcstatus';
+
+module.exports.callback = function (command_name, channel, user, message, object) {
+    request({
+        url: 'http://status.mojang.com/check',
+        json: true,
+        method: 'GET'
+    }, function (err, req, body) {
+        if (err) {
+            return console.error(err);
+        }
+
+        var isUp = true;
+        var whatsDown = [];
+
+        _.forEach(body, function (item) {
+            console.log("a: " + item);
+            for (var key in item) {
+                console.log("b: " + key);
+                console.log("c: " + item[key]);
+                if (item[key] != "green") {
+                    isUp = false;
+                    whatsDown.push(key);
+                }
+            }
+        });
+
+        if (isUp) {
+            connection.client.replyToMessage(user, channel, "All Mojang services seem to be up and running!");
+        } else {
+            connection.client.replyToMessage(user, channel, "The following Mojang services are down: " + whatsDown.join(", "));
+        }
+    });
+};
---
+++ commands/packCommand.js
@@ -0,0 +1,43 @@
+/*
+ * ATLauncherBot - https://github.com/ATLauncher/ATLauncherBot
+ * Copyright (C) 2015 ATLauncher
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+'use strict';
+
+var connection = require('../inc/connection');
+var functions = require('../inc/functions');
+var atlauncherApi = require('atlauncher-api')();
+
+module.exports.enabled = true;
+
+module.exports.name = 'pack';
+
+module.exports.callback = function (command_name, channel, user, message, object) {
+    var parts = functions.getMessageParts(message, 2);
+
+    if (parts.length == 2) {
+        atlauncherApi.pack(parts[1].replace(/[^a-z0-9]+/gi, ""), function (err, pack) {
+            if (err) {
+                console.error(err);
+                connection.client.replyToMessage(user, channel, err.message);
+            } else {
+                var timeAgo = functions.secondsToString(Math.floor(new Date().getTime() / 1000) - pack.versions[0].published);
+                connection.client.replyToMessage(user, channel, pack.name + " is currently version " + pack.versions[0].version + " for Minecraft " + pack.versions[0].minecraft + " and was released " + timeAgo + " ago!");
+            }
+        });
+    }
+};
